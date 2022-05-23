67aa73d60e8c4fefde8d761635a63ebc5e5ce4ce
Andrej Nankov <andrejnankov@gmail.com>
2022-05-23 14:51:45 +0200

    Add craftable admin panel

--- donate2people/webpack.mix.js
+++ donate2people/webpack.mix.js
@@ -15,3 +15,13 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
+
+
+mix
+  .js(["resources/js/admin/admin.js"], "public/js")
+  .sass("resources/sass/admin/admin.scss", "public/css")
+  .vue();
+
+if (mix.inProduction()) {
+  mix.version();
+}
